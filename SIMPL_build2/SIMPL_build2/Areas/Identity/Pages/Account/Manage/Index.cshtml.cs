85d1379cabc8791cebb62e404c056fbed0a854ce
Ryan Geddings <n01067534@ospreys.unf.edu>
2019-04-04 17:09:42 -0400

    Password Reset

--- SIMPL_build2/SIMPL_build2/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ SIMPL_build2/SIMPL_build2/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -4,29 +4,37 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace SIMPL.Areas.Identity.Pages.Account.Manage
 {
-    public partial class IndexModel : PageModel
+   public partial class IndexModel : PageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly IEmailSender _emailSender;
-                
+        private readonly ILogger<IndexModel> _logger;
+        private string userID;
+           private IdentityUser userObj;
+
         public IndexModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            ILogger<IndexModel> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _emailSender = emailSender;
+            _logger = logger;
         }
 
+        
         public string Username { get; set; }
 
         public bool IsEmailConfirmed { get; set; }
@@ -40,7 +48,7 @@ namespace SIMPL.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Required]
+            
             [Display(Name = "IdName")]
             public string IdName { get; set; }
 
@@ -51,21 +59,64 @@ namespace SIMPL.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            
+            [Display(Name ="ID")]
+            public IdentityUser userObj { get; set; }
+            public string userID { get; set; }
+
+            //[Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Current password")]
+            public string OldPassword { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "New password")]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm new password")]
+            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
         }
 
-        public async Task<IActionResult> OnGetAsync(string id)
+        
+
+        public async Task<IActionResult> OnGetAsync(string ID)
         {
+            //
+
             //var user = await _userManager.GetUserAsync(User);
-            var user = await _userManager.FindByIdAsync(id);
-            if (user == null)
+            if ((ID != null)) //&& (userObj == null))
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                HttpContext.Session.SetString("ID", ID);
+                userID = HttpContext.Session.GetString("ID");
+            }
+            else
+            {
+                userID = HttpContext.Session.GetString("ID");
+            }
+            
+            
+            
+            userObj = await _userManager.FindByIdAsync(userID);
+            if ((userID == null)) //&& (userObj == null))
+            {
+                return NotFound($"GET ID value is NULL.");
             }
 
-            var userName = await _userManager.GetUserNameAsync(user);
-            var email = await _userManager.GetEmailAsync(user);
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            //var IdName = await Models.AspNetUsers.;
+            var hasPassword = await _userManager.HasPasswordAsync(userObj);
+            if (!hasPassword)
+            {
+                return RedirectToPage("./SetPassword");
+            }
+
+            var userName = await _userManager.GetUserNameAsync(userObj);
+            var email = await _userManager.GetEmailAsync(userObj);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(userObj);
+            
 
             Username = userName;
 
@@ -73,94 +124,79 @@ namespace SIMPL.Areas.Identity.Pages.Account.Manage
             {
                 Email = email,
                 PhoneNumber = phoneNumber
-                //IdName = IdName     /**See block below**/
+                
             };
 
-            IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
+            IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(userObj);
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string ID)
         {
+            HttpContext.Session.SetString("ID", ID);
+
+            if (ID != null)
+            {
+                userID = HttpContext.Session.GetString("ID");
+            }
+
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+           
 
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
+            IdentityUser userObj = await _userManager.FindByIdAsync(ID);
+            
+            if (userObj == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"POST ID Value is NULL");
             }
 
-            var email = await _userManager.GetEmailAsync(user);
+            var email = await _userManager.GetEmailAsync(userObj);
             if (Input.Email != email)
             {
-                var setEmailResult = await _userManager.SetEmailAsync(user, Input.Email);
+                var setEmailResult = await _userManager.SetEmailAsync(userObj, Input.Email);
                 if (!setEmailResult.Succeeded)
                 {
-                    var userId = await _userManager.GetUserIdAsync(user);
+                    var userId = await _userManager.GetUserIdAsync(userObj);
                     throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
                 }
             }
-/**New block for IDName (Plaintext User Name)
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.IdName != IdName)
-            {
-                var setIdName = await _userManager.SetPhoneNumberAsync(user, Input.IdName);
-                if (!setPhoneResult.Succeeded)
-                {
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting Plaintext Name for user with ID '{userId}'.");
-                }
-            }
-**/
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(userObj);
             if (Input.PhoneNumber != phoneNumber)
             {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(userObj, Input.PhoneNumber);
                 if (!setPhoneResult.Succeeded)
                 {
-                    var userId = await _userManager.GetUserIdAsync(user);
+                    var userId = await _userManager.GetUserIdAsync(userObj);
                     throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
                 }
             }
+            string token = await _userManager.GeneratePasswordResetTokenAsync(userObj);
+            if (token == null)
+            {
+                return NotFound($"Unable to generate Token for User with ID '{_userManager.GetUserIdAsync(userObj)}'.");
+            }
 
-            await _signInManager.RefreshSignInAsync(user);
+            var changePasswordResult = await _userManager.ResetPasswordAsync(userObj, token, Input.NewPassword);
+            if (!changePasswordResult.Succeeded)
+            {
+                foreach (var error in changePasswordResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                
+                return Page();
+            }
+                await _signInManager.RefreshSignInAsync(userObj);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
         }
 
-        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-
-            var userId = await _userManager.GetUserIdAsync(user);
-            var email = await _userManager.GetEmailAsync(user);
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
-                pageHandler: null,
-                values: new { userId = userId, code = code },
-                protocol: Request.Scheme);
-            await _emailSender.SendEmailAsync(
-                email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-            StatusMessage = "Verification email sent. Please check your email.";
-            return RedirectToPage();
-        }
+        
     }
 }
